53677dc5c1ba5a3088505a5359058ab0901e85ae
Melissa <melissa.pacheco.martinez@gmail.com>
2017-08-30 18:12:55 -0300

    proyecto listo

---
+++ assets/js/data.js
@@ -0,0 +1,256 @@
+/*var datosDispositivos =JSON.parse('{
+	
+	"movil_1" : {
+		"mac": "00:28:F8:C9:E7:BB", 
+		"origen1" : {
+			"name" : "Bluecountfirst",
+			"x" : 440,
+			"y": 110,	 
+			"registros" : [
+				{
+					"coordenadaX": 15,
+					"coordenadaY": 15,
+					"hora": "13:25:11",
+					"fecha": "2017-08-11"    
+				},
+				{
+					"coordenadaX": 51,
+					"coordenadaY": 51,
+					"hora": "13:25:47",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 62,
+					"coordenadaY": 62,
+					"hora": "13:25:22",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 79,
+					"coordenadaY": 79,
+					"hora": "13:25:57",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 130,
+					"coordenadaY": 90,
+					"hora": "13:25:08",
+					"fecha": "2017-08-11"
+				}			
+			]
+		},
+		"origen2": {
+			"name" : "Bluecountsecond",
+			"x": 180,
+			"y": 210,
+			"registros":[
+				{
+					"coordenadaX": 47,
+					"coordenadaY": 47,
+					"hora": "13:34:21",
+					"fecha": "2017-08-11"
+					},
+				{
+					"coordenadaX": 31,
+					"coordenadaY": 31,
+					"hora": "13:34:58",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 32,
+					"coordenadaY": 32,
+					"hora": "13:34:59",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 33,
+					"coordenadaY": 33,
+					"hora": "13:34:59",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 31,
+					"coordenadaY": 31,
+					"hora": "13:34:58",
+					"fecha": "2017-08-11"
+				},
+				{
+					"coordenadaX": 33,
+					"coordenadaY": 33,
+					"hora": "13:35:00",
+					"fecha": "2017-08-11"
+				}		
+			]
+		}
+	},
+	"movil_2" : {
+        "mac": "28:16:AD:32:DF:42", 
+        "origen1" : {
+
+            "name" : "Bluecountfirst",
+
+            "x" : 440,
+
+            "y": 110,  
+
+            "registros" : [
+
+                {
+
+                    "coordenadaX": 86,
+
+                    "coordenadaY": 86,
+
+                    "hora": "15:00:54",
+
+                    "fecha": "2017-08-11"    
+
+                },
+
+                {
+
+                    "coordenadaX": 93,
+
+                    "coordenadaY": 93,
+
+                    "hora": "15:00:59",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 89,
+
+                    "coordenadaY": 89,
+
+                    "hora": "15:01:11",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 92,
+
+                    "coordenadaY": 92,
+
+                    "hora": "15:01:15",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 92,
+
+                    "coordenadaY": 92,
+
+                    "hora": "15:01:00",
+
+                    "fecha": "2017-08-11"
+
+                }           
+
+            ]
+
+        },
+
+        "origen2": {
+
+            "name" : "Bluecountsecond",
+
+            "x": 180,
+
+            "y": 210, 
+
+            "registros":[
+
+                {
+
+                    "coordenadaX": 53,
+
+                    "coordenadaY": 53,
+
+                    "hora": "15:00:57",
+
+                    "fecha": "2017-08-11"
+
+                    },
+
+                {
+
+                    "coordenadaX": 51,
+
+                    "coordenadaY": 51,
+
+                    "hora": "15:01:02",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 52,
+
+                    "coordenadaY": 52,
+
+                    "hora": "15:01:12",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 55,
+
+                    "coordenadaY": 55,
+
+                    "hora": "15:01:12",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 55,
+
+                    "coordenadaY": 55,
+
+                    "hora": "15:01:18",
+
+                    "fecha": "2017-08-11"
+
+                },
+
+                {
+
+                    "coordenadaX": 59,
+
+                    "coordenadaY": 59,
+
+                    "hora": "15:01:23",
+
+                    "fecha": "2017-08-11"
+
+                }       
+
+            ]
+
+        }
+
+    }
+
+        
+
+}
+
+		
+} */
+var datosDispositivos = JSON.parse('{"movil_1":{"mac":"00:28:F8:C9:E7:BB","origen1":{"name":"Bluecountfirst","x":440,"y":110,"registros":[{"coordenadaX":15,"coordenadaY":15,"hora":"13:25:11","fecha":"2017-08-11"},{"coordenadaX":51,"coordenadaY":51,"hora":"13:25:47","fecha":"2017-08-11"},{"coordenadaX":62,"coordenadaY":62,"hora":"13:25:22","fecha":"2017-08-11"},{"coordenadaX":79,"coordenadaY":79,"hora":"13:25:57","fecha":"2017-08-11"},{"coordenadaX":130,"coordenadaY":90,"hora":"13:25:08","fecha":"2017-08-11"}]},"origen2":{"name":"Bluecountsecond","x":180,"y":210,"registros":[{"coordenadaX":47,"coordenadaY":47,"hora":"13:34:21","fecha":"2017-08-11"},{"coordenadaX":31,"coordenadaY":31,"hora":"13:34:58","fecha":"2017-08-11"},{"coordenadaX":32,"coordenadaY":32,"hora":"13:34:59","fecha":"2017-08-11"},{"coordenadaX":33,"coordenadaY":33,"hora":"13:34:59","fecha":"2017-08-11"},{"coordenadaX":31,"coordenadaY":31,"hora":"13:34:58","fecha":"2017-08-11"},{"coordenadaX":33,"coordenadaY":33,"hora":"13:35:00","fecha":"2017-08-11"}]}},"movil_2":{"mac":"28:16:AD:32:DF:42","origen1":{"name":"Bluecountfirst","x":440,"y":110,"registros":[{"coordenadaX":86,"coordenadaY":86,"hora":"15:00:54","fecha":"2017-08-11"},{"coordenadaX":93,"coordenadaY":93,"hora":"15:00:59","fecha":"2017-08-11"},{"coordenadaX":89,"coordenadaY":89,"hora":"15:01:11","fecha":"2017-08-11"},{"coordenadaX":92,"coordenadaY":92,"hora":"15:01:15","fecha":"2017-08-11"},{"coordenadaX":92,"coordenadaY":92,"hora":"15:01:00","fecha":"2017-08-11"}]},"origen2":{"name":"Bluecountsecond","x":180,"y":210,"registros":[{"coordenadaX":53,"coordenadaY":53,"hora":"15:00:57","fecha":"2017-08-11"},{"coordenadaX":51,"coordenadaY":51,"hora":"15:01:02","fecha":"2017-08-11"},{"coordenadaX":52,"coordenadaY":52,"hora":"15:01:12","fecha":"2017-08-11"},{"coordenadaX":55,"coordenadaY":55,"hora":"15:01:12","fecha":"2017-08-11"},{"coordenadaX":55,"coordenadaY":55,"hora":"15:01:18","fecha":"2017-08-11"},{"coordenadaX":59,"coordenadaY":59,"hora":"15:01:23","fecha":"2017-08-11"}]}}}');
---
+++ assets/js/route.js
@@ -0,0 +1,55 @@
+/*datosDispositivos.forEach(function(e){
+    $("#show").click(function(){
+                var origen = ""
+                var oporigen = `<option value="${e.movil_1.mac}">${e.movil_1.mac}</option>`;
+                    $("#mac").append(oporigen);
+    })
+})*/
+
+
+document.getElementById('next').addEventListener("click",function(){
+
+  var canvas = document.getElementById('dibujo');
+  var con = canvas.getContext('2d');
+  var con2 = canvas.getContext('2d');
+   rad = 0,
+   rad2 = 0;
+   linW = 0;
+
+    var origenx1 = datosDispositivos.movil_1.origen1.x;
+    var origeny1 = datosDispositivos.movil_1.origen1.y;
+
+    var origenx2 = datosDispositivos.movil_2.origen2.x;
+    var origeny2 = datosDispositivos.movil_2.origen2.y;
+
+
+function draw() { 
+    con.beginPath();
+
+var registro = datosDispositivos.movil_1.origen1.registros.length;
+
+for(var i =0; i < registro; i++){
+
+    var cooX1 = parseInt(origenx1) - parseInt(datosDispositivos.movil_1.origen1.registros[i].coordenadaX);
+    var cooY1 = parseInt(origeny1) + parseInt(datosDispositivos.movil_1.origen1.registros[i].coordenadaY);
+    console.log(cooX1);
+    con.arc(cooX1, cooY1, rad, 0, 2 * Math.PI,false);//Datos de coordenadas dinamicos llamando a las coordenadas del data
+}
+
+ rad++;
+ if(rad == 10) {
+     rad = 1;
+ } else {
+  con.clearRect(0,0,canvas.width,canvas.height);
+ }
+  
+ con.lineWidth = 1;
+ con.strokeStyle = '#99AE04';
+ con.stroke();
+ setTimeout(draw,250);
+}
+draw();
+});
+
+
+
